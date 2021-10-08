d57eea8a7a70c7194276d1bc86f672ba3a7ca213
waqas-younas-deriv <waqas.younas@deriv.com>
2021-10-08 11:35:51 +0500

    code refactoring and changes

--- lib/single_child_layout.dart
+++ lib/single_child_layout.dart
@@ -46,11 +46,30 @@ class ContainerExample extends StatelessWidget {
       appBar: AppBar(),
       body: SafeArea(
         child: Container(
-          width: 100,
+          width: 200,
           height: 100,
-          decoration: BoxDecoration(
-            color: Colors.blue,
+          alignment: Alignment.center,
+          margin: EdgeInsets.only(
+            left: 12,
+            top: 10,
           ),
+          padding: EdgeInsets.only(
+            left: 12,
+          ),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.deepOrange,
+                Colors.indigo,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(5),
+            ),
+          ),
+          child: Text('Container Example'),
         ),
       ),
     );
